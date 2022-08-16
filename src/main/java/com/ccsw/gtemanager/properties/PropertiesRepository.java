95ecb553da1afd03707c6a3b9b810b62b96a8f2f
cavire <108669764+cavire@users.noreply.github.com>
2022-08-16 14:59:04 +0200

    Implementar modelo y repositorio Properties

---
+++ src/main/java/com/ccsw/gtemanager/properties/PropertiesRepository.java
@@ -0,0 +1,9 @@
+package com.ccsw.gtemanager.properties;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ccsw.gtemanager.properties.model.Properties;
+
+public interface PropertiesRepository extends CrudRepository<Properties, Long> {
+
+}
---
+++ src/main/java/com/ccsw/gtemanager/properties/model/Properties.java
@@ -0,0 +1,48 @@
+package com.ccsw.gtemanager.properties.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "properties")
+public class Properties {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "key", nullable = false)
+	private String key;
+
+	@Column(name = "value")
+	private String value;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
