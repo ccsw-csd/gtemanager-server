60699a5a879ffc6cfebe9c29c9a11e0ed9d24cc0
cavire <108669764+cavire@users.noreply.github.com>
2022-08-17 10:12:49 +0200

    Query de búsqueda de Person por saga

--- src/main/java/com/ccsw/gtemanager/person/PersonRepository.java
+++ src/main/java/com/ccsw/gtemanager/person/PersonRepository.java
@@ -3,11 +3,13 @@ package com.ccsw.gtemanager.person;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ccsw.gtemanager.person.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+	@Query("SELECT p FROM Person p LEFT JOIN PersonSagaTranscode s ON p.id = s.personId WHERE p.saga = :saga OR s.saga = :saga")
 	List<Person> findBySaga(String saga);
 
 }
