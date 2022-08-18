9c71f963113e049871d50c9ededa146293006fb3
cavire <108669764+cavire@users.noreply.github.com>
2022-08-18 14:37:42 +0200

    Validación de uploadEvidence y obtener Properties
    - Cambiar return a boolean para validar si se almacenan registros
    pero con errores
    - Declarar getProperties()

--- src/main/java/com/ccsw/gtemanager/evidence/EvidenceService.java
+++ src/main/java/com/ccsw/gtemanager/evidence/EvidenceService.java
@@ -8,19 +8,22 @@ import com.ccsw.gtemanager.evidence.model.EvidenceComment;
 import com.ccsw.gtemanager.evidence.model.EvidenceError;
 import com.ccsw.gtemanager.evidence.model.EvidenceType;
 import com.ccsw.gtemanager.evidence.model.FormDataDto;
+import com.ccsw.gtemanager.properties.model.Properties;
 
 public interface EvidenceService {
 
-	void uploadEvidence(FormDataDto upload) throws IllegalArgumentException, IOException;
-	
+	boolean uploadEvidence(FormDataDto upload) throws IllegalArgumentException, IOException;
+
 	List<Evidence> getEvidences();
-	
+
 	List<EvidenceError> getEvidenceErrors();
-	
+
 	List<EvidenceComment> getEvidenceComments();
-	
+
+	List<Properties> getProperties();
+
 	List<EvidenceType> findEvidenceType(String type);
-	
+
 	String findWeekForPeriod(String period) throws IllegalArgumentException;
-	
+
 }
