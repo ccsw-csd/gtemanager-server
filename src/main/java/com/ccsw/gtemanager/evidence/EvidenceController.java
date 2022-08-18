9ddc53ea0eda06f72c6a41e8a33fd9afe99deaa0
cavire <108669764+cavire@users.noreply.github.com>
2022-08-18 14:39:22 +0200

    Reimplementar validación en EvidenceController
    - Controlar si archivo recibido es válido
    - Eliminar mensaje de debug
    - Validar si se ha subido archivo correcto con errores

--- src/main/java/com/ccsw/gtemanager/evidence/EvidenceController.java
+++ src/main/java/com/ccsw/gtemanager/evidence/EvidenceController.java
@@ -35,30 +35,29 @@ public class EvidenceController {
 	 */
 	@RequestMapping(path = "", method = RequestMethod.POST)
 	public ResponseEntity<String> uploadEvidence(@ModelAttribute FormDataDto upload) {
-//		if (upload.getFile() == null || !StringUtils.hasText(upload.getFile().getContentType()))
-//			return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-//					.body("No se ha recibido un fichero válido.");
+		if (upload.getFile() == null || !StringUtils.hasText(upload.getFile().getContentType()))
+			return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+					.body("No se ha recibido un fichero válido.");
 
-//		if (!upload.getFile().getContentType().equals("application/vnd.ms-excel") && !upload.getFile().getContentType()
-//				.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
-//			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-//					.body("No se ha recibido un fichero de hoja de cálculo.");
+		if (!upload.getFile().getContentType().equals("application/vnd.ms-excel") && !upload.getFile().getContentType()
+				.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.body("No se ha recibido un fichero de hoja de cálculo.");
 
-		System.out
-				.println("file: " + upload.getFile().getOriginalFilename() + " : " + upload.getFile().getContentType());
-		System.out.println("deleteComments: " + upload.isDeleteComments());
-
+		boolean ok;
 		try {
-			evidenceService.uploadEvidence(upload);
+			ok = evidenceService.uploadEvidence(upload);
 		} catch (IllegalArgumentException e) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-					.body(e.getLocalizedMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
 		} catch (Exception e) {
+			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("Se ha producido un error interno. Disculpe las molestias.");
 		}
 
-		return ResponseEntity.status(HttpStatus.OK).body(null);
-
+		if (ok)
+			return ResponseEntity.status(HttpStatus.OK).body(null);
+		else
+			return ResponseEntity.status(HttpStatus.OK).body("[errors]");
 	}
 }
