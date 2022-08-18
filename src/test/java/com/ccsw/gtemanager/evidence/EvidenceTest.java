27ea442bd162948a8162b6fc36d4e4ab929d9f01
cavire <108669764+cavire@users.noreply.github.com>
2022-08-18 13:33:26 +0200

    Refactor: tests unitarios y de integración
    - Eliminar algunos tests unitarios (validados dentro de algunos tests
    de integración)

--- src/test/java/com/ccsw/gtemanager/evidence/EvidenceTest.java
+++ src/test/java/com/ccsw/gtemanager/evidence/EvidenceTest.java
@@ -4,24 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockMultipartFile;
 
-import com.ccsw.gtemanager.evidence.model.FormDataDto;
-import com.ccsw.gtemanager.person.PersonRepository;
+import com.ccsw.gtemanager.properties.PropertiesRepository;
 
 /**
  * EvidenceTest: colección de tests unitarios que prueban funcionalidades de la
@@ -33,130 +22,29 @@ import com.ccsw.gtemanager.person.PersonRepository;
 @ExtendWith(MockitoExtension.class)
 public class EvidenceTest {
 
-	private static final String EXISTING_RUNDATE = "July 27, 2022 08:30 AM";
-	private static final String NONEXISTING_RUNDATE = "asdf";
-
-	private static final String EXISTING_SAGA = "A1";
-	private static final String NONEXISTING_SAGA = "A0";
-
 	private static final String EXISTING_PERIOD = "29-AUG-2022 - 31-AUG-2022";
 	private static final String EXISTING_PERIOD_WEEK = "29-AUG-2022 - 04-SEP-2022";
 	private static final String NONEXISTING_PERIOD = "29-AUG-2022 - 21-AUG-2022";
-
-	private static final String EXISTING_TYPE = "WORKING";
-	private static final String NONEXISTING_TYPE = "Completed";
-
-	private static Workbook gteEvidences;
-	private static Sheet sheet;
-	private static Row row0, row10, row15;
-	private static Cell cellParametersTitle, cellRunDate, cellSAGA;
 
 	@Mock
 	private EvidenceRepository evidenceRepository;
 
 	@Mock
-	private PersonRepository personRepository;
+	private EvidenceErrorRepository evidenceErrorRepository;
+
+	@Mock
+	private EvidenceCommentRepository evidenceCommentRepository;
+
+	@Mock
+	private PropertiesRepository propertiesRepository;
 
 	@InjectMocks
 	private DefaultEvidenceService evidenceService;
 
-	/** */
-	@BeforeAll
-	public static void initializeSpreadsheet() {
-		gteEvidences = new XSSFWorkbook();
-
-		sheet = gteEvidences.createSheet("Sheet1");
-
-		row0 = sheet.createRow(0);
-		cellParametersTitle = row0.createCell(0);
-		cellParametersTitle.setCellValue("Parameters");
-		
-		row10 = sheet.createRow(9);
-		cellRunDate = row10.createCell(1);
-
-		row15 = sheet.createRow(14);
-		cellSAGA = row15.createCell(1);
-	}
-
-	/** */
-	private byte[] exportSpreadsheet() throws IOException {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		gteEvidences.write(baos);
-		return baos.toByteArray();
-	}
-
-	/**
-	 * B10 válido da OK
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void validRunDateShouldReturnOK() throws IOException {
-		cellRunDate.setCellValue(NONEXISTING_RUNDATE);
-
-		FormDataDto upload = new FormDataDto();
-		upload.setFile(new MockMultipartFile("test.xslx", "test.xlsx",
-				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet()));
-		upload.setDeleteComments(false);
-
-		evidenceService.uploadEvidence(upload);
-		assertTrue(!evidenceService.getEvidences().isEmpty());
-	}
-
-	/**
-	 * B10 inválido da error
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void invalidRunDateShouldReturnError() throws IOException {
-		cellRunDate.setCellValue(NONEXISTING_RUNDATE);
-
-		FormDataDto upload = new FormDataDto();
-		upload.setFile(new MockMultipartFile("test.xslx", "test.xlsx",
-				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet()));
-		upload.setDeleteComments(false);
-
-		Exception e = assertThrows(IllegalArgumentException.class, () -> evidenceService.uploadEvidence(upload));
-		assertTrue(e.getLocalizedMessage().contains("[runDate]"));
-	}
-
-	// Username inválido da error
-
-	/**
-	 * Error de proceso debería registrar en evidence_error
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void failedProcessShouldRegisterEvidenceError() throws IOException {
-		cellRunDate.setCellValue(NONEXISTING_RUNDATE);
-
-		FormDataDto upload = new FormDataDto();
-		upload.setFile(new MockMultipartFile("test.xslx", "test.xlsx",
-				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet()));
-		upload.setDeleteComments(false);
-
-		evidenceService.uploadEvidence(upload);
-		assertTrue(!evidenceService.getEvidenceErrors().isEmpty());
-	}
-
-	/** Codsaga existente debería devolver persona */
-	@Test
-	public void existingSAGAShouldReturnPerson() {
-		assertTrue(!personRepository.findBySaga(EXISTING_SAGA).isEmpty());
-	}
-
-	/** Codsaga inexistente debería error */
-	@Test
-	public void nonexistingSAGAShouldReturnError() {
-		assertTrue(personRepository.findBySaga(NONEXISTING_SAGA).isEmpty());
-	}
-
 	/** Periodo correcto debería devolver semana */
 	@Test
 	public void existingPeriodShouldReturnWeek() {
-		assertEquals(evidenceService.findWeekForPeriod(EXISTING_PERIOD), EXISTING_PERIOD_WEEK);
+		assertEquals(EXISTING_PERIOD_WEEK, evidenceService.findWeekForPeriod(EXISTING_PERIOD));
 	}
 
 	/** Periodo incorrecto debería error */
@@ -165,32 +53,19 @@ public class EvidenceTest {
 		assertThrows(IllegalArgumentException.class, () -> evidenceService.findWeekForPeriod(NONEXISTING_PERIOD));
 	}
 
-	/** Tipo correcto debería devolver registro */
-	@Test
-	public void existingTypeShouldReturnEvidenceType() {
-		assertTrue(!evidenceService.findEvidenceType(EXISTING_TYPE).isEmpty());
-	}
-
-	/** Tipo incorrecto debería error */
-	@Test
-	public void nonexistingTypeShouldReturnError() {
-		assertTrue(evidenceService.findEvidenceType(NONEXISTING_TYPE).isEmpty());
-	}
-
-	// Semanas sin datos deberían estar en null
-
 	/** Si deleteComments borrar evidence_comment */
 	@Test
 	public void ifDeleteCommentsEmptyEvidenceComment() {
-		evidenceService.emptyComments();
+		evidenceService.clearEvidenceData(true);
 		assertTrue(evidenceService.getEvidenceComments().isEmpty());
 	}
 
 	/** evidence borrada al comenzar proceso */
 	@Test
 	public void beginningProcessShouldEmptyEvidence() {
-		evidenceService.emptyEvidences();
+		evidenceService.clearEvidenceData(false);
 		assertTrue(evidenceService.getEvidences().isEmpty());
+		assertTrue(evidenceService.getEvidenceErrors().isEmpty());
+		assertTrue(evidenceService.getProperties().isEmpty());
 	}
-
 }
--- src/test/java/com/ccsw/gtemanager/evidence/EvidenceTestIT.java
+++ src/test/java/com/ccsw/gtemanager/evidence/EvidenceTestIT.java
@@ -1,6 +1,7 @@
 package com.ccsw.gtemanager.evidence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -12,8 +13,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -30,7 +29,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import com.ccsw.gtemanager.config.BaseITAbstract;
-import com.ccsw.gtemanager.person.PersonRepository;
 
 /**
  * EvidenceTestIT: colección de tests integrados que prueban funcionalidad del
@@ -46,16 +44,41 @@ public class EvidenceTestIT extends BaseITAbstract {
 	private static final String LOCALHOST = "http://localhost:";
 	private static final String SERVICE_PATH = "/evidence";
 
+	private static final String EXISTING_RUNDATE = "July 27, 2022 08:30 AM";
+	private static final String NONEXISTING_RUNDATE = "asdf";
+
+	private static final String EXISTING_FULLNAME = "Ana Cardo";
+
+	private static final String EXISTING_SAGA = "S_00001";
+	private static final String NONEXISTING_SAGA = "S_00A0";
+
+	private static final String EXISTING_EMAIL = "anacardo@example.com";
+
+	private static final String EXISTING_FROMDATE = "01-AUG-2022";
+	private static final String EXISTING_TODATE = "31-AUG-2022";
+	private static final String NONEXISTING_FROMDATE = "01-SEP-2022";
+	private static final String NONEXISTING_TODATE = "31-JUL-2022";
+
+	private static final String EXISTING_PERIOD = "29-AUG-2022 - 31-AUG-2022";
+	private static final String NONEXISTING_PERIOD = "31-AUG-2022 - 01-AUG-2022";
+
+	private static final String EXISTING_TYPE = "WORKING";
+	private static final String NONEXISTING_TYPE = "Completed";
+
 	@LocalServerPort
 	private int port;
 
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@Autowired
+	private EvidenceService evidenceService;
+
 	private static Workbook gteEvidences;
 	private static Sheet sheet;
-	private static Row row0, row10, row15;
-	private static Cell cellParametersTitle, cellRunDate, cellSAGA;
+	private static Row row0, row1, row2, row9, row14;
+	private static Cell cellParametersTitle, cellFromDate, cellToDate, cellRunDate, cellFullName, cellSAGA, cellEmail,
+			cellPeriod, cellStatus;
 
 	/** */
 	@BeforeAll
@@ -68,11 +91,20 @@ public class EvidenceTestIT extends BaseITAbstract {
 		cellParametersTitle = row0.createCell(0);
 		cellParametersTitle.setCellValue("Parameters");
 
-		row10 = sheet.createRow(9);
-		cellRunDate = row10.createCell(1);
+		row1 = sheet.createRow(1);
+		cellFromDate = row1.createCell(1);
+		row2 = sheet.createRow(2);
+		cellToDate = row2.createCell(1);
 
-		row15 = sheet.createRow(14);
-		cellSAGA = row15.createCell(1);
+		row9 = sheet.createRow(9);
+		cellRunDate = row9.createCell(1);
+
+		row14 = sheet.createRow(14);
+		cellFullName = row14.createCell(0);
+		cellSAGA = row14.createCell(1);
+		cellEmail = row14.createCell(2);
+		cellPeriod = row14.createCell(9);
+		cellStatus = row14.createCell(10);
 	}
 
 	/** */
@@ -87,10 +119,8 @@ public class EvidenceTestIT extends BaseITAbstract {
 	public void sendingElementWithoutTokenShouldReturnError() {
 		String test = "test";
 
-		HttpEntity<?> httpEntity = new HttpEntity<>(test);
-
-		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST, httpEntity,
-				String.class);
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(test), String.class);
 
 		assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
 	}
@@ -100,10 +130,8 @@ public class EvidenceTestIT extends BaseITAbstract {
 	public void sendingInvalidElementShouldReturnError() {
 		String test = "test";
 
-		HttpEntity<?> httpEntity = new HttpEntity<>(test, getHeaders());
-
-		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST, httpEntity,
-				String.class);
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(test, getHeaders()), String.class);
 
 		assertEquals(HttpStatus.UNSUPPORTED_MEDIA_TYPE, response.getStatusCode());
 	}
@@ -121,21 +149,30 @@ public class EvidenceTestIT extends BaseITAbstract {
 		HttpHeaders headers = getHeaders();
 		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-		HttpEntity<?> httpEntity = new HttpEntity<>(body, headers);
-
-		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST, httpEntity,
-				String.class);
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
 
 		assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
 	}
 
 	/**
-	 * Recibir archivo excel debería procesar OK
+	 * Recibir archivo excel con fechas válidas debería procesar OK. existing saga,
+	 * type
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void sendingSpreadsheetFileShouldReturnOK() throws IOException {
+	public void sendingValidSpreadsheetFileShouldReturnOK() throws IOException {
+		cellFromDate.setCellValue(EXISTING_FROMDATE);
+		cellToDate.setCellValue(EXISTING_TODATE);
+		cellRunDate.setCellValue(EXISTING_RUNDATE);
+
+		cellFullName.setCellValue(EXISTING_FULLNAME);
+		cellSAGA.setCellValue(EXISTING_SAGA);
+		cellEmail.setCellValue(EXISTING_EMAIL);
+		cellPeriod.setCellValue(EXISTING_PERIOD);
+		cellStatus.setCellValue(EXISTING_TYPE);
+
 		MockMultipartFile file = new MockMultipartFile("test.xslx", "test.xlsx",
 				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
 
@@ -146,11 +183,176 @@ public class EvidenceTestIT extends BaseITAbstract {
 		HttpHeaders headers = getHeaders();
 		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-		HttpEntity<?> httpEntity = new HttpEntity<>(body, headers);
-
-		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST, httpEntity,
-				String.class);
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(null, response.getBody());
+		assertEquals(1, evidenceService.getEvidences().size());
 	}
+
+	/**
+	 * Fechas inválidas resultan error
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void sendingInvalidSpreadsheetFileShouldReturnError() throws IOException {
+		cellFromDate.setCellValue(EXISTING_FROMDATE);
+		cellToDate.setCellValue(EXISTING_TODATE);
+		cellRunDate.setCellValue(NONEXISTING_RUNDATE);
+
+		cellFullName.setCellValue(EXISTING_FULLNAME);
+		cellSAGA.setCellValue(EXISTING_SAGA);
+		cellEmail.setCellValue(EXISTING_EMAIL);
+		cellPeriod.setCellValue(EXISTING_PERIOD);
+		cellStatus.setCellValue(EXISTING_TYPE);
+
+		MockMultipartFile file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		HttpHeaders headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[date]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+
+		cellFromDate.setCellValue(NONEXISTING_FROMDATE);
+		cellToDate.setCellValue(EXISTING_TODATE);
+		cellRunDate.setCellValue(EXISTING_RUNDATE);
+
+		file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[date]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+
+		cellFromDate.setCellValue(EXISTING_FROMDATE);
+		cellToDate.setCellValue(NONEXISTING_TODATE);
+		cellRunDate.setCellValue(EXISTING_RUNDATE);
+
+		file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[date]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+
+	}
+
+	/**
+	 * Error de proceso debería registrar en evidence_error. nonexisting saga,
+	 * period, type
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void failedProcessShouldRegisterEvidenceError() throws IOException {
+		cellFromDate.setCellValue(EXISTING_FROMDATE);
+		cellToDate.setCellValue(EXISTING_TODATE);
+		cellRunDate.setCellValue(EXISTING_RUNDATE);
+
+		cellFullName.setCellValue(EXISTING_FULLNAME);
+		cellSAGA.setCellValue(NONEXISTING_SAGA);
+		cellEmail.setCellValue(EXISTING_EMAIL);
+		cellPeriod.setCellValue(EXISTING_PERIOD);
+		cellStatus.setCellValue(EXISTING_TYPE);
+
+		MockMultipartFile file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		HttpHeaders headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[errors]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+
+		cellFullName.setCellValue(EXISTING_FULLNAME);
+		cellSAGA.setCellValue(EXISTING_SAGA);
+		cellEmail.setCellValue(EXISTING_EMAIL);
+		cellPeriod.setCellValue(NONEXISTING_PERIOD);
+		cellStatus.setCellValue(EXISTING_TYPE);
+
+		file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[errors]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+		assertEquals(1, evidenceService.getEvidenceErrors().size());
+
+		cellFullName.setCellValue(EXISTING_FULLNAME);
+		cellSAGA.setCellValue(EXISTING_SAGA);
+		cellEmail.setCellValue(EXISTING_EMAIL);
+		cellPeriod.setCellValue(EXISTING_PERIOD);
+		cellStatus.setCellValue(NONEXISTING_TYPE);
+
+		file = new MockMultipartFile("test.xslx", "test.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", exportSpreadsheet());
+
+		body = new LinkedMultiValueMap<>();
+		body.add("file", file.getResource());
+		body.add("deleteComments", false);
+
+		headers = getHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.POST,
+				new HttpEntity<>(body, headers), String.class);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(((String) response.getBody()).contains("[errors]"));
+		assertEquals(0, evidenceService.getEvidences().size());
+		assertEquals(1, evidenceService.getEvidenceErrors().size());
+	}
+
+	// Semanas sin datos deberían estar en null
+
 }
