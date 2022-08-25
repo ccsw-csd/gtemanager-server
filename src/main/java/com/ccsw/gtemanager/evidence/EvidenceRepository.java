3e850d347c4743ce893fe976e7e37ba6f86e654e
Cristal <camacri2@alumni.uv.es>
2022-08-25 08:47:15 +0200

    Añadido entitygraph

--- src/main/java/com/ccsw/gtemanager/evidence/EvidenceRepository.java
+++ src/main/java/com/ccsw/gtemanager/evidence/EvidenceRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ccsw.gtemanager.evidence.model.Evidence;
 
 public interface EvidenceRepository extends CrudRepository<Evidence, Long> {
 
+	@EntityGraph(attributePaths = { "person", "evidenceTypeW1", "evidenceTypeW2", "evidenceTypeW3", "evidenceTypeW4", "evidenceTypeW5", "evidenceTypeW6" })
 	List<Evidence> findAll(Specification<Evidence> specification, Sort by);
 }
