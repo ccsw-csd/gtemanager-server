c395dbf6b273a4e7f400addfec847a456ffc896b
CORP\olagoang <oscar.lago-angel-external@capgemini.com>
2022-09-01 11:09:12 +0200

    GM-03 Cambio de busqueda de personas con specifications

--- src/main/java/com/ccsw/gtemanager/person/PersonServiceImpl.java
+++ src/main/java/com/ccsw/gtemanager/person/PersonServiceImpl.java
@@ -3,10 +3,13 @@ package com.ccsw.gtemanager.person;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ccsw.gtemanager.common.criteria.TernarySearchCriteria;
 import com.ccsw.gtemanager.config.mapper.BeanMapper;
 import com.ccsw.gtemanager.person.model.PersonEntity;
 
@@ -22,8 +25,22 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     public List<PersonEntity> findAllPersonsFromFilters(String filter) {
-        List<PersonEntity> personsLike = this.personRepository.findAllPersonsFromFilters(filter, PageRequest.of(0, 15));
-        return personsLike;
+
+        PersonSpecification username = new PersonSpecification(
+                new TernarySearchCriteria("username", null, null, ":", filter));
+
+        PersonSpecification firstnameLastname = new PersonSpecification(
+                new TernarySearchCriteria("name", "lastName", null, "concat :", filter));
+
+        PersonSpecification lastnameFirstname = new PersonSpecification(
+                new TernarySearchCriteria("lastName", "name", null, "concat :", filter));
+
+        Specification<PersonEntity> specification = Specification.where(username)
+                .or(lastnameFirstname.or(firstnameLastname));
+
+        Page<PersonEntity> personsLike2 = personRepository.findAll(specification, PageRequest.of(0, 15));
+
+        return personsLike2.getContent();
     }
 
 }
---
+++ src/main/java/com/ccsw/gtemanager/person/PersonSpecification.java
@@ -0,0 +1,48 @@
+package com.ccsw.gtemanager.person;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.ccsw.gtemanager.common.criteria.TernarySearchCriteria;
+import com.ccsw.gtemanager.person.model.PersonEntity;
+
+public class PersonSpecification implements Specification<PersonEntity> {
+
+    private static final long serialVersionUID = 1L;
+    private final TernarySearchCriteria criteria;
+
+    public PersonSpecification(TernarySearchCriteria criteria) {
+        this.criteria = criteria;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<PersonEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+        if (criteria.getOperation().equalsIgnoreCase(":") && criteria.getValue() != null) {
+            if (root.get(criteria.getKey()).getJavaType() == String.class) {
+                return criteriaBuilder.like(root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+            } else {
+                return criteriaBuilder.equal(root.get(criteria.getKey()), criteria.getValue());
+            }
+        } else if (criteria.getOperation().equalsIgnoreCase("concat :") && criteria.getValue() != null) {
+            Expression<String> exp = criteriaBuilder.concat(root.<String>get(criteria.getKey()), " ");
+            exp = criteriaBuilder.concat(exp, root.<String>get(criteria.getKey2()));
+            return criteriaBuilder.like(exp, "%" + criteria.getValue() + "%");
+        } else if (criteria.getOperation().equalsIgnoreCase("concat concat :") && criteria.getValue() != null) {
+            Expression<String> exp = criteriaBuilder.concat(root.<String>get(criteria.getKey()), " ");
+            exp = criteriaBuilder.concat(exp, root.<String>get(criteria.getKey2()));
+            exp = criteriaBuilder.concat(exp, " ");
+            exp = criteriaBuilder.concat(exp, root.<String>get(criteria.getKey3()));
+            query.orderBy(criteriaBuilder.asc(root.<String>get(criteria.getKey())),
+                    criteriaBuilder.desc(root.<String>get(criteria.getKey2())));
+            return criteriaBuilder.like(exp, "%" + criteria.getValue() + "%");
+        }
+        return null;
+    }
+
+}
