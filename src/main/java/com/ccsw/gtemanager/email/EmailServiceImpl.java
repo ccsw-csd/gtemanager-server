f3873e3f0722202a995b0e23ee577f4dba9492f1
cavire <108669764+cavire@users.noreply.github.com>
2022-09-14 15:45:25 +0200

    Implementación de envío con Java Mail
    Esta versión de la clase contiene la implementación de envío de
    mensajes a un servidor de correo externo. Eliminar comentarios con TODO,
    configurar String con dirección de remitente, y descomentar llamada al
    método para hacerlo funcionar. Servidor correo configurable en HOST y
    PORT. "localhost:1025" para hacer pruebas a servidor local que simula un
    envío (mailhog: software que simula un servidor local de correo que
    recoge envíos de mensajes y ofrece una interfaz para verlos)

--- src/main/java/com/ccsw/gtemanager/email/EmailServiceImpl.java
+++ src/main/java/com/ccsw/gtemanager/email/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ccsw.gtemanager.email;
 
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -8,22 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.transaction.Transactional;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.ccsw.gtemanager.common.exception.BadRequestException;
-import com.ccsw.gtemanager.email.model.EmailDto;
 import com.ccsw.gtemanager.email.model.ReminderDto;
 import com.ccsw.gtemanager.evidence.EvidenceService;
 import com.ccsw.gtemanager.evidence.model.Evidence;
@@ -33,12 +37,17 @@ import com.ccsw.gtemanager.properties.PropertiesService;
 /**
  * EmailServiceImpl: clase implementación de EmailService. Contiene métodos para
  * la composición y envío de emails de recordatorio.
+ * 
+ * TODO FIXME Esta versión de la clase contiene la implementación de envío de
+ * mensajes a un servidor de correo externo. Eliminar comentarios con TODO,
+ * configurar String con dirección de remitente, y descomentar llamada al método
+ * para hacerlo funcionar. Servidor correo configurable en HOST y PORT.
+ * "localhost:1025" para hacer pruebas a servidor local que simula un envío
+ * (mailhog)
  */
 @Service
 @Transactional
 public class EmailServiceImpl implements EmailService {
-
-    private static final String REMINDER_EMAIL_SUBJECT = "GTE: Recordatorio ";
 
     private static final String REMINDER_TEMPLATE_PATH = "/src/main/resources/reminder-email-template.vm";
 
@@ -52,8 +61,32 @@ public class EmailServiceImpl implements EmailService {
     private static final String ENCODING = "UTF-8";
     private static final String PERIOD_SEPARATOR = " - ";
 
-    @Value("${app.email.url}")
-    private String REMINDER_EMAIL_API_URL;
+    // TODO Variables de configuración de sesión
+    private static final boolean STARTTLS = true;
+    private static final boolean AUTH = false;
+    private static final String PROTOCOL = "TLSv1.2";
+    private static final String SESSION_TRUST = "mail.smtp.ssl.trust";
+    private static final String SESSION_PROTOCOL = "mail.smtp.ssl.protocols";
+    private static final String SESSION_SMTP_PORT = "mail.smtp.port";
+    private static final String SESSION_SMTP_HOST = "mail.smtp.host";
+    private static final String SESSION_ENABLE_STARTTLS = "mail.smtp.starttls.enable";
+    private static final String SESSION_ENABLE_AUTH = "mail.smtp.auth";
+    // TODO content type, importancia del mensaje
+    private static final String REMINDER_EMAIL_CONTENT_TYPE = "text/html; charset=utf-8";
+    private static final String REMINDER_EMAIL_IMPORTANCE_KEY = "Importance";
+    private static final String REMINDER_EMAIL_PRIORITY_KEY = "X-Priority";
+    private static final String REMINDER_EMAIL_IMPORTANCE_VALUE = "High";
+    private static final String REMINDER_EMAIL_PRIORITY_VALUE = "1";
+    // TODO nombre del remitente, dirección del remitente, asunto
+    private static final String REMINDER_EMAIL_NAME = "GTE Manager";
+    private static final String REMINDER_EMAIL_ADDRESS = "[DIRECCIÓN REMITENTE AQUÍ]";
+    private static final String REMINDER_EMAIL_SUBJECT = "GTE: Recordatorio ";
+    // TODO dirección del servidor de correo SMTP y puerto
+    private static final String SMTP_HOST = "localhost";
+    private static final String SMTP_PORT = "1025";
+
+    // TODO separador para nombre del destinatario
+    private static final String NAME_SEPARATOR = ", ";
 
     private static DateTimeFormatter formatDateDB = new DateTimeFormatterBuilder().parseCaseInsensitive()
             .appendPattern("dd/MM/yyyy").toFormatter(Locale.getDefault());
@@ -73,18 +106,42 @@ public class EmailServiceImpl implements EmailService {
         if (evidences.isEmpty())
             throw new BadRequestException("No hay evidencias para el centro seleccionado.");
 
-        return sendAllMessages(REMINDER_EMAIL_API_URL, composeEmails(reminder.getClosingDate(), evidences));
+        // TODO configurar propiedades
+        Properties prop = new Properties();
+        prop.put(SESSION_ENABLE_AUTH, AUTH);
+        prop.put(SESSION_ENABLE_STARTTLS, STARTTLS);
+        prop.put(SESSION_SMTP_HOST, SMTP_HOST);
+        prop.put(SESSION_SMTP_PORT, SMTP_PORT);
+        prop.put(SESSION_PROTOCOL, PROTOCOL);
+        prop.put(SESSION_TRUST, SMTP_HOST);
+
+        Session session = Session
+                .getInstance(prop);/*
+                                    * // TODO getInstance(Properties, Authenticator) para autenticación con
+                                    * contraseña
+                                    * 
+                                    * , new Authenticator() {
+                                    * 
+                                    * @Override protected PasswordAuthentication getPasswordAuthentication() {
+                                    * return new PasswordAuthentication("", ""); } });
+                                    */
+
+        // TODO descomentar para habilitar
+        // return sendAllMessages(composeEmails(session, reminder.getClosingDate(),
+        // evidences));
+        return false;
     }
 
     /**
      * Componer listado de mensajes de correo electrónico (EmailDto) según la fecha
      * de cierre especificada y el centro asociado a las personas.
      * 
+     * @param session     Session para envío de mensajes
      * @param closingDate Fecha de cierre de periodo
      * @param evidences   Listado de evidencias asociadas al centro
-     * @return Listado de EmailDto con los mensajes procesados
+     * @return Listado de Message con los mensajes procesados
      */
-    private List<EmailDto> composeEmails(LocalDate closingDate, List<Evidence> evidences) {
+    private List<Message> composeEmails(Session session, LocalDate closingDate, List<Evidence> evidences) {
         VelocityEngine velocityEngine = new VelocityEngine();
         velocityEngine.init();
 
@@ -101,7 +158,7 @@ public class EmailServiceImpl implements EmailService {
         context.put(REMINDER_TEMPLATE_CLOSING_DATE_PARAM, closingDate.format(formatDateDB));
         context.put(REMINDER_TEMPLATE_WEEKLIST_PARAM, weeks);
 
-        List<EmailDto> messages = new ArrayList<>();
+        List<Message> messages = new ArrayList<>();
 
         for (Evidence evidence : evidences) {
             StringWriter writer = new StringWriter();
@@ -113,28 +170,49 @@ public class EmailServiceImpl implements EmailService {
 
             template.merge(context, writer);
 
-            messages.add(new EmailDto(evidence.getPerson().getEmail(), null, REMINDER_EMAIL_SUBJECT + period,
-                    writer.toString()));
+            // TODO crear y configurar mensaje
+            Message message = new MimeMessage(session);
+            try {
+                // TODO rte
+                message.setFrom(new InternetAddress(REMINDER_EMAIL_ADDRESS, REMINDER_EMAIL_NAME));
+                // TODO destinatario
+                message.setRecipient(Message.RecipientType.TO,
+                        new InternetAddress(evidence.getPerson().getEmail(), String.valueOf(
+                                evidence.getPerson().getLastName() + NAME_SEPARATOR + evidence.getPerson().getName())));
+
+                // TODO asunto y prioridad
+                message.setSubject(REMINDER_EMAIL_SUBJECT + period);
+                message.setHeader(REMINDER_EMAIL_PRIORITY_KEY, REMINDER_EMAIL_PRIORITY_VALUE);
+                message.setHeader(REMINDER_EMAIL_IMPORTANCE_KEY, REMINDER_EMAIL_IMPORTANCE_VALUE);
+
+                // TODO cuerpo del mensaje y content type
+                MimeBodyPart mimeBodyPart = new MimeBodyPart();
+                mimeBodyPart.setContent(writer.toString(), REMINDER_EMAIL_CONTENT_TYPE);
+
+                message.setContent(new MimeMultipart(mimeBodyPart));
+                messages.add(message);
+            } catch (UnsupportedEncodingException | MessagingException e) {
+                e.printStackTrace();
+            }
         }
 
         return messages;
     }
 
     /**
-     * Realizar el envío de mensajes a API en la url especificada con un método
-     * POST.
+     * Realizar el envío de mensajes al servidor de correo.
      * 
-     * @param emailApiUrl URL a la que derivar los mensajes
-     * @param emails      Listado de mensajes a enviar
+     * @param emails Listado de mensajes a enviar
      * @return true si todos los mensajes se han enviado correctamente, false si han
      *         habido errores
      */
-    private boolean sendAllMessages(String emailApiUrl, List<EmailDto> emails) {
+    private boolean sendAllMessages(List<Message> emails) {
         boolean ok = true;
-        RestTemplate restTemplate = new RestTemplate();
-        for (EmailDto message : emails) {
+        for (Message message : emails) {
             try {
-                restTemplate.exchange(emailApiUrl, HttpMethod.POST, new HttpEntity<EmailDto>(message), String.class);
+                // TODO enviar a servidor SMTP a través de Transport
+                // descomentar para habilitar envío
+                // Transport.send(message);
             } catch (Exception e) {
                 ok = false;
             }
